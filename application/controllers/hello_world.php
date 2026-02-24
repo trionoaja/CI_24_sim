ab1a22bace428c908d60bb8b27bf98e53190b871
trionoaja <trionoaja@gmail.com>
2026-02-24 11:10:06 +0700

    comit

---
+++ application/controllers/hello_world.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit ('No direct script access allowed');
+
+class hello_world extends CI_Controller{
+
+    Public function index()
+    {
+        echo "Hello World";
+    }
+}
