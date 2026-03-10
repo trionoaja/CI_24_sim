868e29f322094996fd99046ef8f16d8119beacf6
trionoaja <trionoaja@gmail.com>
2026-03-10 10:51:08 +0700

    update

--- application/controllers/kategori.php
+++ application/controllers/kategori.php
@@ -12,6 +12,10 @@ class kategori extends CI_Controller {
     public function index()
     {
         $data['kategori']=$this->kategori_model->get_all();
+		$this->load->view('templates/header');
+        $this->load->view('templates/topbar');
+        $this->load->view('templates/sidebar');
         $this->load->view('kategori/index', $data);
+		$this->load->view('templates/footer');
     }
 }
---
+++ application/views/templates/sidebar.php
@@ -0,0 +1,43 @@
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-info sidebar sidebar-dark accordion" id="accordionSidebar">
+
+  <!-- Brand -->
+  <a class="sidebar-brand d-flex align-items-center justify-content-center" 
+  href="<?= site_url('dashboard') ?>">
+    <div class="sidebar-brand-icon rotate-15">
+      <i class="fas fa-laugh-wink"></i>
+    </div>
+    <div class="sidebar-brand-text rotate-n-15 mx-3">CI3 <sup>SB</sup></div>
+  </a>
+
+  <hr class="sidebar-divider my-0">
+
+  <!-- Dashboard -->
+  <li class="nav-item active">
+    <a class="nav-link" href="#">
+      <i class="fas fa-fw fa-tachometer-alt"></i>
+      <span>Dashboard</span>
+    </a>
+  </li>
+
+  <hr class="sidebar-divider">
+
+  <!-- Menu Barang -->
+  <li class="nav-item">
+    <a class="nav-link" href="<?= site_url('kategori') ?>">
+      <i class="fas fa-box"></i>
+      <span>Kategori</span>
+    </a>
+  </li>
+
+
+  <hr class="sidebar-divider d-none d-md-block">
+
+</ul>
+<!-- End of Sidebar -->
+
+<!-- Content Wrapper -->
+<div id="content-wrapper" class="d-flex flex-column">
+
+  <!-- Main Content -->
+  <div id="content">
