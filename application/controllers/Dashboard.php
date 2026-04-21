a372ef458c98aa18f7babcdbbfc5a16369b59c09
trionoaja <trionoaja@gmail.com>
2026-04-21 10:36:40 +0700

    pertemuan 8

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH')OR  exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+
+    }
+
+    public function index()
+    {
+        $data['total_buku'] = $this->db->count_all('buku');
+        $data['total_anggota']= $this->db->count_all('anggota');
+        // $data['total_peminjaman']= $this->db->count_all('peminjaman');
+
+        $this->load->view('templates/header');
+        $this->load->view('templates/sidebar');
+        $this->load->view('templates/topbar');
+        $this->load->view('dashboard/index', $data);
+        $this->load->view('templates/footer');
+    }
+}
---
+++ application/views/dashboard/index.php
@@ -0,0 +1,37 @@
+<h1 class="h3 mb-4 text-gray-800">Dashboard</h1>
+
+<div class="row">
+
+    <!-- Buku -->
+    <div class="col-xl-4 col-md-6 mb-4">
+        <div class="card border-left-primary shadow h-100 py-2">
+            <div class="card body">
+                <h5>Total Buku</h5>
+                <h3><?= $total_buku; ?></h3>
+            </div>
+        </div>
+    </div>
+    <!-- anggota -->
+    <div class="col-xl-4 col-md-6 mb-4">
+        <div class="card border-left-primary shadow h-100 py-2">
+            <div class="card body">
+                <h5>Total Anggota</h5>
+                <h3><?= $total_anggota; ?></h3>
+            </div>
+        </div>
+    </div>
+    <!-- Peminjaman -->
+    <!-- <div class="col-xl-4 col-md-6 mb-4">
+        <div class="card border-left-primary shadow h-100 py-2">
+            <div class="card body">
+                <h5>Total Peminjaman</h5>
+                <h3><?= $total_peminjaman; ?></h3>
+            </div>
+        </div>
+    </div> -->
+    <div class="card shadow mb-4">
+        <div class="card-body">
+            <canvas id="chartDashboard"></canvas>
+        </div>
+    </div>
+</div>
